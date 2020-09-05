4b563c7c52bf0d150fb719c352770474bebce04c
Pasha2407 <pasha.khimchuk2@gmail.com>
2020-09-05 20:40:53 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/inf.css">
+	<title>Good Time</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<div class="form" >	
+		<form>	
+<img src="images/header.png" class = "header">
+
+			<div class="pro">
+				<a href = "#" class = "p">Про</a>
+				<img src = "images/pro.png" class="icon">
+			</div>
+			<div class="game">
+				<a href = "game.html" class="g">Ігри</a>
+				<img src = "images/game.png" class="icon">
+			</div>
+			<div class="nastav">
+							<a href = "https://www.instagram.com/pablodesign108/?hl=ru" class = "n" target="_blank">Для наставника</a>
+							<img src = "images/nastav.png" class="icon">
+			</div>
+			<div class="read">
+				<a href = "#" class = "r">Прочитай</a>
+				<img src = "images/read.png" class="icon">
+			</div>
+			<div class="shop">
+				<a href = "#" class = "sh">Магазин</a>
+				<img src = "images/shop.png" class="icon">
+			</div>
+			<div class="idea">
+				<a href = "#" class = "i">Ідеї</a>
+				<img src = "images/idea.png" class="icon">
+			</div>
+			<div class="photo">
+				<a href = "#" class = "ph">Картинки</a>
+				<img src = "images/photo.png" class="icon">
+			</div>
+			<div class="contact">
+				<a href = "#" class = "c">Контакти</a>
+				<img src = "images/contact.png" class="icon">
+			</div>
+			<div class="setting">
+				<a href = "#" class="s">Налаштування</a>
+				<img src = "images/setting.png" class="icon">
+			</div>
+		</form>
+</div>
+</body>
+</html>
