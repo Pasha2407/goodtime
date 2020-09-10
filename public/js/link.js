95f026b36fc70a51a44d9188ca4ea2495dcbd957
Pasha2407 <70815888+Pasha2407@users.noreply.github.com>
2020-09-10 18:44:57 +0300

    Адаптивність,Текст,Обнова коду.
    Доробив адаптивність меню,плиток,текста під всі пристроЇ ;
    Доробив текст ;
    Іконки построєні через список , замість силок js , переробив в плитках верхнє меню , всьо підстроїв під % для адаптивності.

---
+++ public/js/link.js
@@ -0,0 +1,42 @@
+/* MENU ---------------------- */
+
+function pro(){
+	document.location.href = "pro.html";
+}
+function game(){
+	document.location.href = "game.html";
+}
+function nastav(){
+	document.location.href = "nastav.html";
+}
+function read(){
+ document.location.href = "read.html";
+}
+function shop(){
+	document.location.href = "shop.html";
+}
+function idea(){
+  document.location.href = "idea.html";
+}
+function images(){
+  document.location.href = "images.html";
+}
+function gameday(){
+  document.location.href = "gameday.html";
+}
+function setting(){
+  document.location.href = "setting.html";
+}
+
+/* BACK --------------------- */
+
+function back_menu(){
+  document.location.href = "index.html";
+}
+  function back_game(){
+    document.location.href = "../game.html";
+  }
+    function back_sport(){
+      document.location.href = "../sportuvni.html";
+    }
+
